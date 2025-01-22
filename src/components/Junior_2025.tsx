f1bbc4d7f1278225824478a63e0f5577060d8c82
Jesse Born <jesse.born@gmx.ch>
2025-01-22 18:31:19 +0100

    add junior randomization

--- src/components/Junior_2025.tsx
+++ src/components/Junior_2025.tsx
@@ -3,15 +3,15 @@ import "./Randomization.css";
 import { MarbleBag, rand } from "~/utils";
 import { makePersisted } from "@solid-primitives/storage";
 export default function Junior2025() {
-  const [state, setState] = makePersisted(createSignal(""), { name: "jr24" });
+  const [state, setState] = makePersisted(createSignal(""), { name: "jr25" });
   const svg = (
     <svg class="rand-image" viewBox="0 0 1000 485" height="485" width="1000" preserveAspectRatio="xMidYMid meet">
-      <rect width="16" height="16" x="225" y="443" fill={IndColor(state(), 0)} stroke="#fff" stroke-width={4} />
-      <rect width="16" height="16" x="260" y="443" fill={IndColor(state(), 1)} stroke="#fff" stroke-width={4} />
-      <rect width="16" height="16" x="297" y="443" fill={IndColor(state(), 2)} stroke="#fff" stroke-width={4} />
-      <rect width="16" height="16" x="332" y="443" fill={IndColor(state(), 3)} stroke="#fff" stroke-width={4} />
-      <rect width="16" height="16" x="370" y="443" fill={IndColor(state(), 4)} stroke="#fff" stroke-width={4} />
-      <rect width="16" height="16" x="405" y="443" fill={IndColor(state(), 5)} stroke="#fff" stroke-width={4} />
+      {<rect width="32" height="32" x="430" y="120" fill={getColorFromCode(state(), 0)} stroke={"#000"} stroke-width={4} />}
+      {<rect width="32" height="32" x="430" y="160" fill={getColorFromCode(state(), 1)} stroke={"#000"} stroke-width={4} />}
+      {<rect width="32" height="32" x="430" y="200" fill={getColorFromCode(state(), 2)} stroke={"#000"} stroke-width={4} />}
+      {<rect width="32" height="32" x="430" y="240" fill={getColorFromCode(state(), 3)} stroke={"#000"} stroke-width={4} />}
+      {<rect width="32" height="32" x="430" y="280" fill={getColorFromCode(state(), 4)} stroke={"#000"} stroke-width={4} />}
+      {<rect width="32" height="32" x="430" y="320" fill={getColorFromCode(state(), 5)} stroke={"#000"} stroke-width={4} />}
       Sorry but this browser does not support inline SVG.
     </svg>
   );
@@ -33,20 +33,23 @@ export default function Junior2025() {
   );
 
   function getRandomState() {
-    const ind1Bag = new MarbleBag(["T", "T", "F", "F", "L", "L"]);
+    const ind1Bag = new MarbleBag(["R", "G", "Y", "W", "X", "X"]);
 
-    setState(`JR24-${ind1Bag.draw()}${ind1Bag.draw()}${ind1Bag.draw()}${ind1Bag.draw()}${ind1Bag.draw()}${ind1Bag.draw()}`);
+    setState(`JR25-${ind1Bag.draw()}${ind1Bag.draw()}${ind1Bag.draw()}${ind1Bag.draw()}${ind1Bag.draw()}${ind1Bag.draw()}`);
   }
 }
 
-function IndColor(code: string, index: number) {
+function getColorFromCode(code: string, index: number) {
   try {
     const letter = code.split("-")[1].charAt(index);
-    if (letter === "T") return "green";
-    if (letter === "F") return "red";
-    if (letter === "L") return "blue";
+    if (letter === "G") return "green";
+    if (letter === "W") return "white";
+    if (letter === "Y") return "yellow";
+    if (letter === "R") return "red";
+    if (letter === "X") return "rgba(0,0,0,0)";
   } catch {}
 }
+
 function callWithDecreasingVelocity(func: Function, initialDelay: number, decrement: number, minDelay: number) {
   let delay = initialDelay;
 
